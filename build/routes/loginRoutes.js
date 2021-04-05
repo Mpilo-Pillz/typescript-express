e999748a170ffac071126878dbfbc001e25204d1
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-05 15:52:44 +0200

    adding login route

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -4,6 +4,10 @@ exports.router = void 0;
 var express_1 = require("express");
 var router = express_1.Router();
 exports.router = router;
-router.get("/", function (req, res) {
-    res.send("skhipani");
+router.get("/login", function (req, res) {
+    res.sendFile("login.html", { root: __dirname });
+});
+router.post("/login", function (req, res) {
+    var _a = req.body, email = _a.email, password = _a.password;
+    res.send(email + password);
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,15 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("skhipani");
+router.get("/login", (req: Request, res: Response) => {
+  res.sendFile("login.html", { root: __dirname });
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  res.send(email + password);
 });
 
 export { router };
