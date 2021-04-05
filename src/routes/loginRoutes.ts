eca9ccf0f6496f403758b32128e83479ffe4da0d
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-05 16:00:13 +0200

    adding custom interface to deal with the lack of types in the type definition file

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,15 +1,23 @@
 import { Router, Request, Response } from "express";
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get("/login", (req: Request, res: Response) => {
   res.sendFile("login.html", { root: __dirname });
 });
 
-router.post("/login", (req: Request, res: Response) => {
+router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  res.send(email + password);
+  if (email && password) {
+    res.send(email + password);
+  } else {
+    res.send("you must provide an email and password");
+  }
 });
 
 export { router };
