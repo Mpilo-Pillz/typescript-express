f0fa9001c851552fcf60f3ebaed13f3573d4d01a
Fikasentani <mpilo.dlamini@yahoo.com>
2021-04-14 06:24:26 +0200

    adding enums to avoid typos for routes

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
+import { Methods } from "./Methods";
 // import express from "express";
 
 // export const router = express.Router(); deleted this beciase we now only have one instance coming from AppRouter
@@ -11,9 +12,15 @@ export function controller(routePrefix: string) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
       const path = Reflect.getMetadata("path", target.prototype, key);
+      const method: Methods = Reflect.getMetadata(
+        "method",
+        target.prototype,
+        key
+      );
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
+        // router.get(`${routePrefix}${path}`, routeHandler);
       }
     }
   };
